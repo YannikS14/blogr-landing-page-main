efde74695d3ed55b729946f41cbfc4cf7b272ccf
Yannik Scheibner <6142228+YannikS14@users.noreply.github.com>
2021-04-16 20:52:07 +0200

    Add nav dropdown menus

---
+++ script.js
@@ -0,0 +1,32 @@
+const navButtons = document.querySelectorAll('.nav-btn');
+const navLists = document.querySelectorAll('.nav-list');
+
+navButtons.forEach((el) => {
+  el.addEventListener('click', (event) => {
+    const clickedNavList = document.querySelector(
+      `#${event.target.id} + .nav-list`,
+    );
+    const clickedButton = document.getElementById(event.target.id);
+
+    removeActiveClass();
+
+    clickedNavList.classList.toggle('active');
+    clickedButton.classList.toggle('active');
+  });
+});
+
+document.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('nav-btn')) {
+    removeActiveClass();
+  }
+});
+
+const removeActiveClass = () => {
+  navLists.forEach((el) => {
+    el.classList.remove('active');
+  });
+
+  navButtons.forEach((el) => {
+    el.classList.remove('active');
+  });
+};
